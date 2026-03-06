4bb5ecf5e7d15b784f01d7e6ce1fb99c40397891
Antonio Vandré Pedrosa Furtunato Gomes <123699192+antoniovandre@users.noreply.github.com>
2026-03-06 16:03:11 -0300

    Add files via upload

--- Math/antoniovandre_eval.c
+++ Math/antoniovandre_eval.c
@@ -2,7 +2,7 @@
 
 // Retorna a avaliação (eval) da expressão passada como argumento, versão de distribuição.
 
-// Licença de uso: Atribuição-NãoComercial-CompartilhaIgual (CC BY-NC-SA).
+// Licença de uso: Creative Commons Atribuição (CC BY).
 
 // Última atualização: 22-02-2026. Não considerando alterações em variáveis globais.
 
