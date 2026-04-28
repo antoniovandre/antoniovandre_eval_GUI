f073bed30d58ff39240609ad3688e83c194f3336
Antonio Vandré Pedrosa Furtunato Gomes <123699192+antoniovandre@users.noreply.github.com>
2026-04-28 11:30:02 -0300

    Add files via upload

--- antoniovandre_evalGUI.c
+++ antoniovandre_evalGUI.c
@@ -6,7 +6,7 @@
 
 // Licença de uso: Creative Commons Atribuição (CC BY).
 
-// Última atualização: 24-04-2026. Não considerando alterações em variáveis globais.
+// Última atualização: 28-04-2026. Não considerando alterações em variáveis globais.
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,7 +18,7 @@
 
 #include "antoniovandre_constantes.c"
 
-#define VERSION 20260424
+#define VERSION 20260428
 #define MENSAGEMNAOCOMPILADOR "Software não compilado em razão do compilador não ser compatível."
 #define NUMEROZERO 0
 #define NUMEROUM 1
@@ -8038,6 +8038,23 @@ char * antoniovandre_evalcelulafuncao (char * str, int precisao)
 				tc = VARIAVELDESUBSTITUICAO4;
 				return antoniovandre_numeroparastring ((int) tc, precisao);
 				}
+			else if (! strcmp (str2, "operadorsoma"))
+				{
+				if (str2 != NULL) free (str2);
+
+				if (MACROALOCACAODINAMICA)
+					{
+					if (buffer != NULL) free (buffer);
+
+					for (i = NUMEROZERO; i < TAMANHO_BUFFER_FUNCOESCONSTANTES; i++)
+						{if (funcoesconstantes [i].token != NULL) free (funcoesconstantes [i].token); if (funcoesconstantes [i].comentario != NULL) free (funcoesconstantes [i].comentario);}
+
+					if (funcoesconstantes != NULL) free (funcoesconstantes);
+					}
+
+				tc = OPERADORSOMA;
+				return antoniovandre_numeroparastring ((int) tc, precisao);
+				}
 			else if (! strcmp (str2, "operadorsubtracao"))
 				{
 				if (str2 != NULL) free (str2);
@@ -8630,7 +8647,7 @@ char * antoniovandre_evalcelula (char * str, int precisao)
 					break;
 					}
 
-				if ((strt [posicoes_operadores [i]] == '+') && (flag == NUMEROZERO) && (flag2 == NUMEROZERO))
+				if ((strt [posicoes_operadores [i]] == OPERADORSOMA) && (flag == NUMEROZERO) && (flag2 == NUMEROZERO))
 					{
 					valor = (TIPONUMEROREAL) valort + (TIPONUMEROREAL) valort2;
 					if ((valor > VALOR_MAX) || (valor < (NUMEROMENOSUM) * VALOR_MAX)) {if (str != NULL) free (str); if (MACROALOCACAODINAMICA) {if (strt4 != NULL) free (strt4); if (strtv2 != NULL) free (strtv2); if (strtv1 != NULL) free (strtv1); if (strt3 != NULL) free (strt3); if (strt2 != NULL) free (strt2);} char * result = (char *) malloc (TAMANHO_BUFFER_PHRASE); if (result == NULL) {char * r = (char *) malloc (NUMEROUM); r [NUMEROZERO] = CARACTEREFIMSTRING; return r;} antoniovandre_copiarstring (result, STRINGSAIDAERROOVER); return result;}
@@ -8843,7 +8860,7 @@ char * antoniovandre_eval (char * str, int precisao)
 						contador--;
 					}
 
-					if ((! strncmp (str2 + i, "mediaaritmetica", 15)) || (! strncmp (str2 + i, "mediaaritmeticaponderada", 24)) || (! strncmp (str2 + i, "mediageometrica", 15)) || (! strncmp (str2 + i, "mediaharmonica", 14)) || (! strncmp (str2 + i, "variancia", 9)) || (! strncmp (str2 + i, "desviopadrao", 12)) || (! strncmp (str2 + i, "sum", 3)) || (! strncmp (str2 + i, "prod", 4)) || (! strncmp (str2 + i, "der", 3)) || (! strncmp (str2 + i, "int", 3)) || (! strncmp (str2 + i, "raizes", 6)) || (! strncmp (str2 + i, "seigual", 7)) || (! strncmp (str2 + i, "sediferente", 11)) || (! strncmp (str2 + i, "semaior", 6)) || (! strncmp (str2 + i, "semenor", 7)) || (! strncmp (str2 + i, "semaiorouigual", 14)) || (! strncmp (str2 + i, "semenorouigual", 14)) || (! strncmp (str2 + i, "base", 4)))
+					if ((! strncmp (str2 + i, "mediaaritmetica", 15)) || (! strncmp (str2 + i, "mediaaritmeticaponderada", 24)) || (! strncmp (str2 + i, "mediageometrica", 15)) || (! strncmp (str2 + i, "mediaharmonica", 14)) || (! strncmp (str2 + i, "variancia", 9)) || (! strncmp (str2 + i, "desviopadrao", 12)) || (! strncmp (str2 + i, "sum", 3)) || (! strncmp (str2 + i, "prod", 4)) || (! strncmp (str2 + i, "der", 3)) || (! strncmp (str2 + i, "int", 3)) || (! strncmp (str2 + i, "raizes", 6)) || (! strncmp (str2 + i, "seigual", 7)) || (! strncmp (str2 + i, "sediferente", 11)) || (! strncmp (str2 + i, "semaior", 6)) || (! strncmp (str2 + i, "semenor", 7)) || (! strncmp (str2 + i, "semaiorouigual", 14)) || (! strncmp (str2 + i, "semenorouigual", 14)) || (! strncmp (str2 + i, "pitagoras", 9)) || (! strncmp (str2 + i, "diagonalparalelepipedo", 22)) || (! strncmp (str2 + i, "pat", 3)) || (! strncmp (str2 + i, "par", 3)) || (! strncmp (str2 + i, "pas", 3)) || (! strncmp (str2 + i, "pasr", 4)) || (! strncmp (str2 + i, "pgt", 3)) || (! strncmp (str2 + i, "prg", 3)) || (! strncmp (str2 + i, "pgs", 3)) || (! strncmp (str2 + i, "pgsr", 4)) || (! strncmp (str2 + i, "mmc", 3)) || (! strncmp (str2 + i, "mdc", 3)) || (! strncmp (str2 + i, "max", 3)) || (! strncmp (str2 + i, "min", 3)) || (! strncmp (str2 + i, "base", 4)))
 						if (flag2 == NUMEROZERO)
 							{
 							if (flag2 == NUMEROZERO)
